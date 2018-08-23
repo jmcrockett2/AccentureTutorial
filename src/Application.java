6d53c9f35e3a0794d5c7a6bb50f25ed0e02d3f9b
Jenn Crockett <jmcrockett2@gmail.com>
2018-08-22 19:28:09 -0500

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,28 @@
+import static java.lang.System.out;
+
+public class Application
+{
+    public static void main(String[] args)
+    {
+        int myNumber = 88;
+        short myShort = 847;
+        long myLong = 9797;
+
+        double myDouble = 7.3243;
+        float myFloat = 324.3f;
+
+        char myChar = 'y';
+        boolean myBoolean = false;
+
+        byte myByte = 127;
+
+        out.println(myNumber);
+        out.println(myShort);
+        out.println(myLong);
+        out.println(myDouble);
+        out.println(myFloat);
+        out.println(myChar);
+        out.println(myBoolean);
+        out.println(myByte);
+    }
+}
